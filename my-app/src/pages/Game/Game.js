8dfe9140999f19423677538cbdfe0ac9b9194522
hsiehja <joannahsieh2003@gmail.com>
2025-02-10 11:11:16 -0600

    game page routes in Game.js

--- my-app/src/pages/Game/Game.js
+++ my-app/src/pages/Game/Game.js
@@ -1,12 +1,24 @@
+// Game.js
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+
+// Import your 3 sub-pages
+import GameModeSelect from "../../components/GameModeSelect/GameModeSelect";
+import GenreSelect from "../../components/GenreSelect/GenreSelect";
+import PlayGame from "../../components/PlayGame/PlayGame";
+
 import "./Game.css";
 
 const Game = () => {
   return (
     <div className="game-container">
       <Navbar />
-      {/* TODO - GameModeSelect, GenreSelect, PlayGame components here */}
+      <Routes>
+        <Route index element={<GameModeSelect />} />
+        <Route path="genres" element={<GenreSelect />} />
+        <Route path="play" element={<PlayGame />} />
+      </Routes>
     </div>
   );
 };
