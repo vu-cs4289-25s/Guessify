57c7487ffdbbbe9bd55e7a57dbb5704e28f7c7ff
hsiehja <joannahsieh2003@gmail.com>
2025-02-10 11:01:51 -0600

    static game pages

---
+++ my-app/src/pages/Game/Game.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Game.css";
+
+const Game = () => {
+  return (
+    <div className="game-container">
+      <Navbar />
+      {/* TODO - GameModeSelect, GenreSelect, PlayGame components here */}
+    </div>
+  );
+};
+
+export default Game;
