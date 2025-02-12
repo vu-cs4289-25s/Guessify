fed0f99692422e25adad5d3e21c1062b98902f12
hsiehja <joannahsieh2003@gmail.com>
2025-02-12 12:31:23 -0600

    sizing

--- my-app/src/pages/Profile/Profile.js
+++ my-app/src/pages/Profile/Profile.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import "./Profile.css";
 
 const Profile = () => {
   return (
-    <div>
+    <div className="profile-container">
       <Navbar />
-      <h1>Your Profile</h1>
+      <div className="profile-overlays-wrapper">
+        {/* Team panel */}
+        <div className="profile-overlay-panel">
+          <h2 className="profile-title">PROFILE</h2>
+          {/* TODO - spotify profile pic */}
+          {/* TODO - spotify user */}
+          <div className="profile-content">
+            <p>1,300 Free Pixel Icons</p>
+            <p>Pixel Icons</p>
+            <p>Spotify API</p>
+            <p>More External Sources</p>
+          </div>
+        </div>
+        {/* TODO - logout button centered */}
+        {/* Sources panel */}
+        <div className="profile-overlay-panel">
+          <h2 className="profile-generate-poster">GENERATE POSTER</h2>
+        </div>
+        {/* TODO - share button centered */}
+      </div>
     </div>
   );
 };
