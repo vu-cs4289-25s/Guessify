3f98ff99633ce066e06cce6e060170d16523e7d4
hsiehja <joannahsieh2003@gmail.com>
2025-03-09 21:54:33 -0500

    added high score for each genre

--- my-app/src/components/HowToPlayOverlay/HowToPlayOverlay.js
+++ my-app/src/components/HowToPlayOverlay/HowToPlayOverlay.js
@@ -5,38 +5,38 @@ const HowToPlayOverlay = ({ isOpen, onClose }) => {
   if (!isOpen) return null; // Don't render if not open
 
   return (
-    <div className="how-to-play-overlay-container">
-      <div className="how-to-play-overlay">
-        {/* <button className="close-button" onClick={onClose}>
+    <div className="overlay-how-to-play-overlay-container">
+      <div className="overlay-how-to-play-overlay">
+        {/* <button className="overlay-close-button" onClick={onClose}>
           ✖
         </button> */}
-        <h1 className="how-to-title">HOW TO PLAY</h1>
-        <div className="how-to-icons-container">
-          <div className="how-to-icon-box">
+        <h1 className="overlay-how-to-title">HOW TO PLAY</h1>
+        <div className="overlay-how-to-icons-container">
+          <div className="overlay-how-to-icon-box">
             <img
               src="/HTPIcons/HTP_listen.png"
               alt="Listen to the song"
-              className="how-to-icon"
+              className="overlay-how-to-icon"
             />
-            <p className="how-to-description">LISTEN TO THE SONG</p>
+            <p className="overlay-how-to-description">LISTEN TO THE SONG</p>
           </div>
-          <div className="how-to-icon-box">
+          <div className="overlay-how-to-icon-box">
             <img
               src="/HTPIcons/HTP_guess.png"
               alt="Guess the song name"
-              className="how-to-icon"
+              className="overlay-how-to-icon"
             />
-            <p className="how-to-description">
+            <p className="overlay-how-to-description">
               GUESS THE SONG NAME IN LIMITED TIME
             </p>
           </div>
-          <div className="how-to-icon-box">
+          <div className="overlay-how-to-icon-box">
             <img
               src="/HTPIcons/HTP_point.png"
               alt="Earn points"
-              className="how-to-icon"
+              className="overlay-how-to-icon"
             />
-            <p className="how-to-description">
+            <p className="overlay-how-to-description">
               EARN POINTS & LAND ON LEADERBOARD
             </p>
           </div>
